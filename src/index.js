db3fb24b4069d3ae68023f3caf240311834ca0fe
bonjeanj <bonjeanj@gmail.com>
2018-04-23 22:07:57 -0500

    added more squres

--- src/index.js
+++ src/index.js
@@ -27,16 +27,31 @@ function Square(props) {
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(3)}
-            {this.renderSquare(4)}
             {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(9)}
+            {this.renderSquare(10)}
+            {this.renderSquare(11)}
+            {this.renderSquare(12)}
+            {this.renderSquare(13)}
+            {this.renderSquare(14)}
+            {this.renderSquare(15)}
+            {this.renderSquare(16)}
+          </div>
+          <div className="board-row">
+          {this.renderSquare(17)}
+            {this.renderSquare(18)}
+            {this.renderSquare(19)}
+            {this.renderSquare(20)}
+            {this.renderSquare(21)}
+            {this.renderSquare(22)}
+            {this.renderSquare(23)}
+            {this.renderSquare(24)}
           </div>
         </div>
       );
@@ -129,14 +144,14 @@ function Square(props) {
   
   function calculateWinner(squares) {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
+      [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      [9, 10, 11, 12, 13, 14, 15, 16],
+      [17, 18, 19, 20, 21, 22, 23, 24],
+      [25, 26, 27, 28, 29, 30, 31, 32],
+      [33, 34, 35, 36, 37, 38, 39, 40],
+      [41, 42, 43, 44, 45, 46, 47, 48],
+      [49, 50, 51, 52, 53, 54, 55, 56],
+      [57, 58, 59, 60, 61, 62, 63, 64]
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
