373f5fbefe2bfebb95fc0e19a0db971d3e0b5f7d
bonjeanj <bonjeanj@gmail.com>
2018-05-14 21:58:46 -0500

    Added all chess pieces, added pieces in chess start posistion, added images

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ function Square(props) {
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-        if(i==0)  {
-        this.props.squares[i] = <img src={ require('./resources/horse.png')} />;   }
+    constructor(props){
+      super(props); 
+    }
 
+    renderSquare(i) {
+      if(this.props.squares[i] == 'H') {  this.props.squares[i] = <img src={ require('./resources/horse.png')} /> } 
+      if(this.props.squares[i] == 'P') {  this.props.squares[i] = <img src={ require('./resources/creepy-yoda.jpg')} /> } 
+      if(this.props.squares[i] == 'R') {  this.props.squares[i] = <img src={ require('./resources/vader.jpeg')} /> } 
       return (
          <Square 
           value={this.props.squares[i]}
@@ -33,12 +37,13 @@ function Square(props) {
             {this.renderSquare(1)}
             {this.renderSquare(2)}
             {this.renderSquare(3)}
+            {this.renderSquare(4)}
             {this.renderSquare(5)}
             {this.renderSquare(6)}
             {this.renderSquare(7)}
-            {this.renderSquare(8)}
           </div>
           <div className="board-row">
+            {this.renderSquare(8)}
             {this.renderSquare(9)}
             {this.renderSquare(10)}
             {this.renderSquare(11)}
@@ -46,67 +51,66 @@ function Square(props) {
             {this.renderSquare(13)}
             {this.renderSquare(14)}
             {this.renderSquare(15)}
-            {this.renderSquare(16)}
           </div>
           <div className="board-row">
-          {this.renderSquare(17)}
+          {this.renderSquare(16)}
+            {this.renderSquare(17)}
             {this.renderSquare(18)}
             {this.renderSquare(19)}
-            {this.renderSquare(20)}
+            {this.renderSquare(29)}
             {this.renderSquare(21)}
             {this.renderSquare(22)}
             {this.renderSquare(23)}
-            {this.renderSquare(24)}
           </div>
           <div className="board-row">
-          {this.renderSquare(17)}
-            {this.renderSquare(18)}
-            {this.renderSquare(19)}
-            {this.renderSquare(20)}
-            {this.renderSquare(21)}
-            {this.renderSquare(22)}
-            {this.renderSquare(23)}
-            {this.renderSquare(24)}
+          {this.renderSquare(24)}
+            {this.renderSquare(25)}
+            {this.renderSquare(26)}
+            {this.renderSquare(27)}
+            {this.renderSquare(28)}
+            {this.renderSquare(29)}
+            {this.renderSquare(30)}
+            {this.renderSquare(31)}
           </div>
           <div className="board-row">
-          {this.renderSquare(17)}
-            {this.renderSquare(18)}
-            {this.renderSquare(19)}
-            {this.renderSquare(20)}
-            {this.renderSquare(21)}
-            {this.renderSquare(22)}
-            {this.renderSquare(23)}
-            {this.renderSquare(24)}
+          {this.renderSquare(32)}
+            {this.renderSquare(33)}
+            {this.renderSquare(34)}
+            {this.renderSquare(35)}
+            {this.renderSquare(36)}
+            {this.renderSquare(37)}
+            {this.renderSquare(38)}
+            {this.renderSquare(39)}
           </div>
           <div className="board-row">
-          {this.renderSquare(17)}
-            {this.renderSquare(18)}
-            {this.renderSquare(19)}
-            {this.renderSquare(20)}
-            {this.renderSquare(21)}
-            {this.renderSquare(22)}
-            {this.renderSquare(23)}
-            {this.renderSquare(24)}
+          {this.renderSquare(40)}
+            {this.renderSquare(41)}
+            {this.renderSquare(42)}
+            {this.renderSquare(43)}
+            {this.renderSquare(44)}
+            {this.renderSquare(45)}
+            {this.renderSquare(46)}
+            {this.renderSquare(47)}
           </div>
           <div className="board-row">
-          {this.renderSquare(17)}
-            {this.renderSquare(18)}
-            {this.renderSquare(19)}
-            {this.renderSquare(20)}
-            {this.renderSquare(21)}
-            {this.renderSquare(22)}
-            {this.renderSquare(23)}
-            {this.renderSquare(24)}
+          {this.renderSquare(48)}
+            {this.renderSquare(49)}
+            {this.renderSquare(50)}
+            {this.renderSquare(51)}
+            {this.renderSquare(52)}
+            {this.renderSquare(53)}
+            {this.renderSquare(54)}
+            {this.renderSquare(55)}
           </div>
           <div className="board-row">
-          {this.renderSquare(17)}
-            {this.renderSquare(18)}
-            {this.renderSquare(19)}
-            {this.renderSquare(20)}
-            {this.renderSquare(21)}
-            {this.renderSquare(22)}
-            {this.renderSquare(23)}
-            {this.renderSquare(24)}
+          {this.renderSquare(56)}
+            {this.renderSquare(57)}
+            {this.renderSquare(58)}
+            {this.renderSquare(59)}
+            {this.renderSquare(60)}
+            {this.renderSquare(61)}
+            {this.renderSquare(62)}
+            {this.renderSquare(63)}
           </div>
         </div>
       );
@@ -116,10 +120,17 @@ function Square(props) {
   class Game extends React.Component {
     constructor(props) {
       super(props);
+      const array = Array(32).fill(null); 
+      const backrow = ['R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R' ]
+      const pawnRow = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' ]
+      let temp = backrow.concat(pawnRow); 
+      temp = temp.concat(array); 
+      temp = temp.concat(pawnRow); 
+      const initalChessBoard = temp.concat(backrow); 
       this.state = {
         history: [
           {
-            squares: Array(9).fill(null)
+            squares: initalChessBoard
           }
         ],
         stepNumber: 0,
@@ -171,11 +182,11 @@ function Square(props) {
         );
       });
   
-      let status;
-      if (winner) {
-        status = "Winner: " + winner;
-      } else {
-        status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
       }
   
       return (
